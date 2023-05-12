882d988765716ac16a221f0e65d398aa009ca3fc
Sulav12345 <daagyat@gmail.com>
2023-05-12 02:08:36 +0100

    UWL-26 <VERSION2 File instead of Image>

---
+++ magazine_front/converter/lib/pages/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+ class Userhome extends StatelessWidget{
+   const Userhome ({Key? key}) : super(key:key);
+   @override
+    Widget build(BuildContext context) {
+    return  Center(
+        child: Text(
+          'PDF CONVERTER',
+          style: TextStyle(fontSize: 50),
+        )
+      );
+      
+      
+  }
+ }
